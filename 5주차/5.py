a078cb79053f77a97f57c53f76928323982e0284
yoojeongg <162241401+yoojeongg@users.noreply.github.com>
2026-04-02 15:35:28 +0900

    Update 5.py

--- 5주차/5.py
+++ 5주차/5.py
@@ -2,12 +2,12 @@ a=0
 b=0
 gugudanLine=""
 for a in range (2,10, 1):
-  gugudanLine = gugudanLine + (" # %d단 # " %a)
+  gugudanLine = gugudanLine + (" # %d단 #\t" %a)
 print(gugudanLine)
 
 for a in range (1,10, 1):
   gugudanLine=""  
   for b in range (2, 10, 1):
-    gugudanLine = gugudanLine + str("%dX %d= %.2d "%(b,a,a*b))
+    gugudanLine = gugudanLine + str("%dX %d= %.2d \t"%(b,a,a*b))
   print(gugudanLine)
   
